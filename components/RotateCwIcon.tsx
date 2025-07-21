a81cef87fa8e9cff563bdea7878ab52e529560c3
Marv22-hue <40erhobene_himmel@icloud.com>
2025-07-21 23:39:50 +0200

    Create RotateCwIcon.tsx

---
+++ components/RotateCwIcon.tsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const RotateCwIcon: React.FC = () => (
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor" 
+        strokeWidth="2" 
+        strokeLinecap="round" 
+        strokeLinejoin="round" 
+        className="lucide lucide-rotate-cw"
+    >
+        <path d="M21 2v6h-6"/>
+        <path d="M3 12a9 9 0 1 1 9 9 9.75 9.75 0 0 1-6.74-2.74L3 12"/>
+    </svg>
+);
+
+export default RotateCwIcon;
