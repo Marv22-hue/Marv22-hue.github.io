21ccc89522b0c05d6dccd9d66b7ad89a8544852a
Marv22-hue <40erhobene_himmel@icloud.com>
2025-07-21 23:39:42 +0200

    Create Ball.tsx

---
+++ components/Ball.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import { ColorName } from '../types';
+import { BALL_SIZE_PX, COLOR_MAP } from '../constants';
+
+interface BallProps {
+  yPosition: number;
+  colorName: ColorName;
+}
+
+const Ball: React.FC<BallProps> = ({ yPosition, colorName }) => {
+  const colorClasses = COLOR_MAP[colorName];
+
+  return (
+    <div
+      className={`absolute left-1/2 -translate-x-1/2 rounded-full filter ${colorClasses.glow}`}
+      style={{
+        top: `${yPosition}px`,
+        width: `${BALL_SIZE_PX}px`,
+        height: `${BALL_SIZE_PX}px`,
+      }}
+    >
+        <div className={`w-full h-full rounded-full ${colorClasses.bg}`}></div>
+    </div>
+  );
+};
+
+export default Ball;
